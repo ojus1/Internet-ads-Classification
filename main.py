d55766925ac7d8b81d8c3dd2eb250e6dd4008233
ojus1 <surya.oju@gmail.com>
2018-03-30 05:52:18 +0530

    Fixed buggy code

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import Imputer
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 
 f = open('ads_data.txt','r')
 lines = f.readlines()
@@ -17,5 +19,18 @@ df = pd.get_dummies(df, columns= [1558], drop_first= True)
 imp = Imputer(strategy= 'mean', axis=0)
 df = imp.fit_transform(df)
 
-X = df.drop(columns= [1558])
-Y = df[[1558]]
+X = np.delete(df,1558,1)
+Y = df[:,1558]
+Y = Y.reshape(3279,1)
+
+Scores = list()
+
+for i in range(0,50) :
+    X_tr, X_ts, Y_tr, Y_ts = train_test_split(X,Y)
+
+    model = SVC()
+    model.fit(X_tr,Y_tr)
+    Scores.append(model.score(X_ts,Y_ts))
+
+plt.plot(Scores)
+plt.show()
